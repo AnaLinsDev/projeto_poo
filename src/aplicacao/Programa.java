19a0d4b9e82ec4945205e6c22bc2fca82bdadaa2
8b1tz <yohannaoliveira12@gmail.com>
2020-11-12 17:41:41 -0300

    teste e cadastro

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,24 @@
 package aplicacao;
 
+import java.util.ArrayList;
+
+import fachada.ClasseFachada;
+import modelo.Produto;
+
 public class Programa {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		ClasseFachada.cadastrarProduto("TV", 2000.0);
+		ClasseFachada.cadastrarProduto("BONECA", 3000.0);
+		String texto;
+		ArrayList<Produto> lista1 = ClasseFachada.listarProdutos();
+		texto = "\nListagem de produtos: \n";
+		if (lista1.isEmpty())
+			texto += "não tem produto cadastrado\n";
+		else 	
+			for(Produto p: lista1) 
+				texto +=  p + "\n"; 
+		System.out.println(texto);
 
 	}
 
