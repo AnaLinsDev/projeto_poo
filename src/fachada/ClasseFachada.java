324dd90dad36845884fb638dfd3c4ab99e7ec8a2
8b1tz <yohannaoliveira12@gmail.com>
2020-11-12 16:41:27 -0300

    listar check

--- src/fachada/ClasseFachada.java
+++ src/fachada/ClasseFachada.java
@@ -1,6 +1,22 @@
 package fachada;
 
+import java.util.ArrayList;
+
 import modelo.Cliente;
+import modelo.Pedido;
+import modelo.Produto;
+import repositorio.Repositorio;
 
 public class ClasseFachada {
+	public static Repositorio repositorio = new Repositorio();
+
+	public ArrayList<Produto> listarProdutos(){
+		return repositorio.getProdutos();
+	}
+	public ArrayList<Cliente> listarClientes(){
+		return repositorio.getClientes();
+	}
+	public ArrayList<Pedido> listarPedidos(){
+		return repositorio.getPedidos();
+	}
 }
