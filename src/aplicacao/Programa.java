4badf5da35516f7cf3b707213245f48fcd8f6770
AnaJL <analinsdev@gmail.com>
2020-11-13 17:02:12 -0300

    commit 5

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -15,8 +15,8 @@ public class Programa {
 			Fachada.cadastrarProduto("TV", 2000.0);
 			Fachada.cadastrarProduto("BONECA", 3000.0);
 			Fachada.cadastrarProduto("CARRO", 2000.0);
-			Fachada.cadastrarProduto("BOLA", 3000.0);
-			Fachada.cadastrarProduto("CELULAR", 2000.0);
+			Fachada.cadastrarProduto("BOLA", 3500.0);
+			Fachada.cadastrarProduto("CELULAR", 2500.0);
 			
 			Fachada.cadastrarCliente("982828282", "Ana Julia", "Brasil");
 			Fachada.cadastrarCliente("982845454", "Yohanna", "Japão");
@@ -33,7 +33,6 @@ public class Programa {
 			// Adicionando produtos nos pedidos
 			
 			Fachada.adicionarProdutoPedido(1, 1);
-			/*
 			Fachada.adicionarProdutoPedido(2, 2);
 			Fachada.adicionarProdutoPedido(3, 3);
 			Fachada.adicionarProdutoPedido(4, 4);
@@ -43,7 +42,6 @@ public class Programa {
 			Fachada.adicionarProdutoPedido(3, 3);
 			Fachada.adicionarProdutoPedido(4, 2);
 			Fachada.adicionarProdutoPedido(5, 1);
-			*/
 			
 			// Pagando entregadores para pedido 1 e 2 + cancelando o 3
 
@@ -52,6 +50,11 @@ public class Programa {
 			Fachada.getPedidoById(2).setEntregador("Jose");
 			Fachada.getPedidoById(2).setPago(true);
 			Fachada.cancelarPedido(3);
+			
+			Fachada.getPedidoById(1).setValortotal(Fachada.getPedidoById(1).geraValortotal());
+			Fachada.getPedidoById(2).setValortotal(Fachada.getPedidoById(2).geraValortotal());;
+			Fachada.getPedidoById(4).setValortotal(Fachada.getPedidoById(4).geraValortotal());;
+			Fachada.getPedidoById(5).setValortotal(Fachada.getPedidoById(5).geraValortotal());;
 			
 			// Listar Produtos
 			texto = "\nListagem de "+ Fachada.listarProdutos("").size() + " produtos:\n";
